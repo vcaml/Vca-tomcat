210f7fecd555d1c72cc06efe6e026878d60557ae
DESKTOP-CFTT1MO\vcaml <734886783@qq.com>
2021-09-02 12:09:51 +0800

    add the customizedClassLoader

--- src/cn/vcaml/vcatomcat/Bootstrap.java
+++ src/cn/vcaml/vcatomcat/Bootstrap.java
@@ -1,11 +1,28 @@
 package cn.vcaml.vcatomcat;
 import cn.vcaml.vcatomcat.catalina.Server;
+import cn.vcaml.vcatomcat.classloader.CommonClassLoader;
+
+import java.lang.reflect.Method;
 
 public class Bootstrap {
 
-    public static void main(String[] args) {
-            Server server  = new Server();
-            server.start();
+    public static void main(String[] args) throws Exception {
+        CommonClassLoader commonClassLoader = new CommonClassLoader();
+
+        Thread.currentThread().setContextClassLoader(commonClassLoader);
+
+        String serverClassName = "cn.vcaml.vcatomcat.catalina.Server";
+
+        Class<?> serverClazz = commonClassLoader.loadClass(serverClassName);
+
+        Object serverObject = serverClazz.newInstance();
+
+        Method m = serverClazz.getMethod("start");
+
+        m.invoke(serverObject);
+
+        // 不能关闭，否则后续就不能使用啦
+        // commonClassLoader.close();
     }
 
 }
